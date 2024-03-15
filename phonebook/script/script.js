278238146d2ed2790d92c106f7faf27daf018f86
unknown <git@github.com:Risha661/CRM.git>
2024-03-15 09:01:34 +0300

    Далее

--- phonebook/script/script.js
+++ phonebook/script/script.js
@@ -37,20 +37,52 @@
     const mainContainer = createContainer();
     main.append(mainContainer);
     main.mainContainer = mainContainer;
-    return main;
-  }
 
+    return main;
+  };
+
+const createButtonsGroup = params => {
+  const btnWrapper = document.createElement('div');
+  btnWrapper.classList.add('btn-wrapper');
+
+  const btns = params.map(({className, type, text}) => {
+    const button = document.createElement('button');
+    button.type = type;
+    button.text = text;
+    button.className = className;
+    return button;
+  });
+  btnWrapper.append(...btns);
+
+  return {
+    btnWrapper, 
+    btns,
+  };
+};
 
   const init = (selectorApp, title) => {
     const app = document.querySelector(selectorApp);
     const header = createHeader();
     const logo = createLogo(title);
+    const main = createMain();
+    const buttonGroup = createButtonsGroup([
+      {
+        className: 'btn btn-primary mr-3',
+        type: 'button',
+        text: 'Добавить',
+      },
+      {
+        className: 'btn btn-danger',
+        type: 'button',
+        text: 'Удалить',
+      },
+    ]);
 
     header.headerContainer.append(logo);
-    
-    app.append(header);
+    main.mainContainer.append(buttonGroup.btnWrapper);
+    app.append(header, main);
   
-  }
+  };
 
   window.phoneBookInit = init;
 }

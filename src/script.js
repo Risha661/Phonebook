15ca2720e48a63e64fe44597bef22e43cd42a1fa
unknown <git@github.com:Risha661/CRM.git>
2024-07-02 18:12:51 +0300

    Правки кода

--- src/script.js
+++ src/script.js
@@ -3,9 +3,6 @@ import hoverRow from "./script/control";
 import { renderPhoneBook, renderContacts } from "./script/render";
 import { data } from "./script/serviceStorage";
 
-// import "./css/style.css";
-// import "./css/bootstrap.min.css";
-// import "./css/normalize.min.css";
 import "./scss/index.scss";
 
 {
